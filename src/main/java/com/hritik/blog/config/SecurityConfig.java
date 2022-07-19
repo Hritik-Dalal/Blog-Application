9e91b5e54068c869aa25fff4f04b0f430dbdb8d3
Hritik Dalal <dalalhritik555@gmail.com>
2022-07-19 11:27:32 +0530

    completed till 33th video

--- src/main/java/com/hritik/blog/config/SecurityConfig.java
+++ src/main/java/com/hritik/blog/config/SecurityConfig.java
@@ -1,19 +1,29 @@
 package com.hritik.blog.config;
 
 import com.hritik.blog.security.CustomUserDetailService;
+import com.hritik.blog.security.JwtAuthenticationFilter;
+import com.hritik.blog.security.JwtAutheticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAutheticationEntryPoint jwtAutheticationEntryPoint;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Autowired
     private CustomUserDetailService customUserDetailService;
@@ -23,10 +33,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
              .csrf().disable()
              .authorizeHttpRequests()
+             .antMatchers("/api/v1/auth/login").permitAll()
              .anyRequest()
              .authenticated()
              .and()
-             .httpBasic();
+             .exceptionHandling().authenticationEntryPoint(this.jwtAutheticationEntryPoint)
+             .and().sessionManagement()
+             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -37,5 +52,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
